fbd7f57f22d4a32e917d8afb122e07f42bbc0859
wilma <wilmanjoel@hotmail.com>
2026-04-12 14:24:45 -0500

    eureka

--- cars/src/main/java/com/wil/cars/CarsApplication.java
+++ cars/src/main/java/com/wil/cars/CarsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class CarsApplication {
 
 	public static void main(String[] args) {
---
+++ eurekaserver/src/test/java/com/wil/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wil.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
