895331d78737cbfd1c6d278dd92d674f947b08e7
wilma <wilmanjoel@hotmail.com>
2026-04-12 23:16:52 -0500

    gateway

--- cars/src/main/java/com/wil/cars/controller/CarController.java
+++ cars/src/main/java/com/wil/cars/controller/CarController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/api/v1/car")
 public class CarController {
     
     @Autowired
